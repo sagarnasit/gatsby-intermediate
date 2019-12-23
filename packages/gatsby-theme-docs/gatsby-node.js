673982302c71c0e6563240a80eb6d440325e26e4
sagarnasit <sagarnasit@gmail.com>
2019-12-23 22:00:42 +0530

    feat(theme-dev): Create pages with Mdx files

--- packages/gatsby-theme-docs/gatsby-node.js
+++ packages/gatsby-theme-docs/gatsby-node.js
@@ -77,3 +77,32 @@ exports.createResolvers = ({ createResolvers }) => {
         }
     })
 }
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
+    const result = await graphql(`
+        query {
+            allDocsPage {
+                nodes {
+                    id
+                    path
+                }
+            }
+        }
+    `);
+
+    if (result.errors) {
+        reporter.panic('Errors loading docs', result.errors);
+    }
+
+    const pages = result.data.allDocsPage.nodes;
+    console.log(pages);
+    pages.forEach(page => {
+        actions.createPage({
+            path: page.path,
+            component: require.resolve('./src/templates/docs-page-template.js'),
+            context: {
+                pageId: page.id,
+            },
+        })
+    });
+}
