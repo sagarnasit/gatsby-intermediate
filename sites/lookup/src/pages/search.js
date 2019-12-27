e975ab1a299fdc46af9016c192afc8a0b6035b65
sagarnasit <sagarnasit@gmail.com>
2019-12-27 22:20:45 +0530

    feat(lookup): Set up client side routing

---
+++ sites/lookup/src/pages/search.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const Search = () => (
+    <>
+        <h1>Search Rick & Morty Character</h1>
+        <p>
+            Find some cool Rick and Morty characters.
+        </p>
+
+    </>
+);
+
+export default Search;
